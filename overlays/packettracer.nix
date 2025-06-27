5d3fd5b2b58f9ce9db0c0ca3156c798bb03c3993
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-06-27 18:09:42 +1000

    overlay: add stripXml helper

--- overlays/packettracer.nix
+++ overlays/packettracer.nix
@@ -1,4 +1,4 @@
-## Packettracer uses some legacy shit because the updated binary looks for new tar.gz
+## Uses legacy libxml2 for Cisco Packet Tracer
 # overlays/packettracer.nix
 final: prev:
 
@@ -8,13 +8,15 @@ let
     url    = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz";
     sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
   }) { inherit (prev) system; };
+
+  stripXml = list: builtins.filter (pkg: !(pkg ? pname && pkg.pname == "libxml2")) list;
 in
 {
   # patch the real build
   ciscoPacketTracer8-unwrapped = prev.ciscoPacketTracer8-unwrapped.overrideAttrs (old: {
     # drop the new libxml2, add the old one, give patchelf its path
-    buildInputs       = (old.buildInputs or []) ++ [ legacy.libxml2 ];
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ prev.autoPatchelfHook ];
+    buildInputs       = stripXml (old.buildInputs or []) ++ [ legacy.libxml2 ];
+    nativeBuildInputs = stripXml (old.nativeBuildInputs or []) ++ [ prev.autoPatchelfHook ];
     autoPatchelfSearchPath = [ "${legacy.libxml2}/lib" ];
   });
 }
