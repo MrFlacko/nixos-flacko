22da726cf918e5237a47632ab1d86bc15a5c5efa
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-06-28 21:09:57 +1000

    Delete overlays directory

--- overlays/packettracer.nix
+++
@@ -1,8 +0,0 @@
-# overlays/packettracer.nix
-(
-  final: prev:
-  let legacyPkgs = import (
-    builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz"
-    ) { inherit (prev) system config; };
-  in { ciscoPacketTracer8 = legacyPkgs.ciscoPacketTracer8; }
-)
