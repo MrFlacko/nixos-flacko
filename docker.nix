bc672360bf7beb829c729d4d0b4d79d0911aa4e0
MrFlacko <joshuaflack123@gmail.com>
2025-07-03 19:27:22 +1000

    Added Docker
    Docker addition is complete.
    Added docker.nix file and a toggle swith within configuration.nix
    Added flacko to the group docker. Should have the separate.

--- docker.nix
+++ docker.nix
@@ -1,5 +1,29 @@
 { config, pkgs, cmod, ... }:
 
 {
-  environment.shellAliases.testvar = "echo Success!!";
+  ## Made with https://nixos.wiki/wiki/Docker
+
+  # Enable Docker
+  virtualisation.docker.enable = true;
+
+  # Add the user to the group docker (Groups don't apply unless you restart)
+  users.users.flacko.extraGroups = [ "docker" ];
+
+  # # This can also be done with docker for the socket
+  # users.extraGroups.docker.members = [ "username-with-access-to-socket" ];
+
+  # Using Docker in Rootless mode
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+  # Installing Docker NixOS
+  environment.systemPackages = with pkgs; [
+    docker_28 docker-compose
+  ];
+
+  virtualisation.docker.daemon.settings = {
+    data-root = "/mnt/8TBDrive/VMs/Docker";
+  };
 }
