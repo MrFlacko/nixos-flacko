5b2363fc634db183d0f41f185f6179430160fb04
MrFlacko <joshuaflack123@gmail.com>
2025-07-06 13:27:26 +1000

    Added ProtonGE
    Added protonGE big download to steaam

--- Packages/steam.nix
+++ Packages/steam.nix
@@ -7,12 +7,13 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraCompatPackages = with pkgs; [ proton-ge-bin ]; # Adding proton-ge-bin
   };
 
   # 32-bit drivers (needed for Proton)
   hardware.graphics.enable32Bit = true;
   hardware.nvidia.modesetting.enable = true;
-  
+
   # optional: Proton-GE updater
   environment.systemPackages = with pkgs; [
     protonup-qt gamescope
