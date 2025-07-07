1e829eb6852a870d250a008a8bf9e85bb36dbecd
MrFlacko <joshuaflack123@gmail.com>
2025-07-07 23:23:35 +1000

    Blackscreen Xorg Support
    Modified the blackscreen script to add support for xorg and wayland
    Tested on xorg, not tested on wayland yet.
    Detection may not be correct
    
    [[ $XDG_SESSION_TYPE == wayland || -n $WAYLAND_DISPLAY ]] && wayland

--- Scripts/blackscreen.sh
+++ Scripts/blackscreen.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-sleep 1 && \
-dbus-send --session --print-reply \
-  --dest=org.kde.kglobalaccel \
-  /component/org_kde_powerdevil \
-  org.kde.kglobalaccel.Component.invokeShortcut \
-  string:'Turn Off Screen'
+xorg() { sleep 1; xset dpms force off; }
+
+wayland() {
+  sleep 1;
+  dbus-send --session --print-reply \
+    --dest=org.kde.kglobalaccel \
+    /component/org_kde_powerdevil \
+    org.kde.kglobalaccel.Component.invokeShortcut \
+    string:'Turn Off Screen'
+}
+
+[[ $XDG_SESSION_TYPE == wayland || -n $WAYLAND_DISPLAY ]] && wayland || xorg
