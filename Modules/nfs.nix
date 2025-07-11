0751529e036af1ae32090b97ac8511f0ee92a81c
MrFlacko <joshuaflack123@gmail.com>
2025-07-11 19:07:12 +1000

    Smoothing out NFS RAM Usage

--- Modules/nfs.nix
+++ Modules/nfs.nix
@@ -5,8 +5,20 @@
 
 {
   fileSystems."/mnt/alfred" = {
-    device = "172.16.0.10:/mnt/big";
+    device  = "172.16.0.10:/mnt/big";
     fsType  = "nfs4";
-    options = [ "rw" "hard" "intr" ];
+    options = [
+      "rw" "hard" "intr"
+      "vers=4.2"
+      "rsize=1048576" "wsize=1048576"
+      "nconnect=4"
+    ];
+  };
+  
+  # Smoothing out RAM usage
+  boot.kernel.sysctl = {
+    "vm.dirty_writeback_centisecs" = "100";
+    "vm.dirty_ratio"               = "15";
+    "vm.dirty_background_ratio"    = "10";
   };
 }
