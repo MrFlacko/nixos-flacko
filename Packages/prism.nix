477ef8976156e8fbfb51583256e158653fd2a0d4
MrFlacko <joshuaflack123@gmail.com>
2026-06-15 17:18:49 +1000

    Prism unstable fix to not use stable kde-open

--- Packages/prism.nix
+++ Packages/prism.nix
@@ -1,9 +1,15 @@
 { config, lib, pkgs, modulesPath, cmod, ... }:
 let
   unstable = import <nixos-unstable> {};
+  prismFixed = pkgs.writeShellScriptBin "prismlauncher" ''
+    unset QT_PLUGIN_PATH
+    unset QML2_IMPORT_PATH
+    unset NIXPKGS_QT6_QML_IMPORT_PATH
+    exec ${unstable.prismlauncher}/bin/prismlauncher "$@"
+  '';
 in
 {
-  environment.systemPackages = with pkgs; [
-    unstable.prismlauncher
+  environment.systemPackages = [
+    prismFixed
   ];
 }
